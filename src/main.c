c2dababbd7b4740f190d5dcab544aaa5ee215824
Bryan Huhta <br.huhta@gmail.com>
2019-09-12 00:16:54 -0500

    Cleaned main.c

--- src/main.c
+++ src/main.c
@@ -1,24 +1,23 @@
-// #include "stm32f0xx_conf.h"
-#include "gpio_driver.h" 
 #include <stdio.h>
 #include <stdbool.h>
-#include "utilities.h"
-#include "stm32f0xx_conf.h"
+
+#include "gpio_driver.h"
 #include "RGB_driver.h"
+#include "stm32f0xx_conf.h"
 #include "stm32f0xx_tim.h"
-#include "stm32f0xx_tim.h"
+#include "utilities.h"
 
 int main(void)
 {
-	SystemInit();
-	gpio_init();
-	utilities_init();
-	RGB_init();
+    SystemInit();
+    gpio_init();
+    utilities_init();
+    RGB_init();
 
-	// Example usage
-	send_string("Hello World!\n");
-	write_RGB(250,1000,750);
-	write_debug_led(1000);
+    // Example usage
+    send_string("Hello World!\n");
+    write_RGB(250,1000,750);
+    write_debug_led(1000);
 
-	while(1);
+    while (1);
 }
